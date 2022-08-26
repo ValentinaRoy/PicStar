dfb543e411f0240dd12b9aab7adba65ae3379277
Valentina Roy <98744375+ValentinaRoy@users.noreply.github.com>
2022-08-26 21:49:57 +0530

    Add files via upload

---
+++ backend/middlewares/sendEmail.js
@@ -0,0 +1,21 @@
+const nodeMailer = require("nodemailer");
+
+exports.sendEmail = async (options) => {
+  const transporter = nodeMailer.createTransport({
+    host: "smtp.mailtrap.io",
+    port: 2525,
+    auth: {
+      user: "b2e091931a9e84",
+      pass: "734e4d0b3f73fd"
+    },
+  });
+
+  const mailOptions = {
+    from: process.env.SMPT_MAIL,
+    to: options.email,
+    subject: options.subject,
+    text: options.message,
+  };
+
+  await transporter.sendMail(mailOptions);
+};
